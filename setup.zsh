94770e09485c40a9e52ed55204909c7b6b13c56a
Barrett K. Harber <barrett.harber@gmail.com>
2016-12-21 21:15:47 -0500

    Add home repos to clone list, add swp files to global gitignore

--- setup.zsh
+++ setup.zsh
@@ -4,10 +4,22 @@ dotfiles=${0:A:h}
 
 ln -s $dotfiles/.bash_profile ~/.bash_profile
 
-#export GH_USERNAME = "binoculars"
-#export PROJECTS_DIR = ~/Projects
-#export HOME = $PROJECTS_DIR/home
+GH_USERNAME="binoculars"
+PROJECTS_DIR=~/Projects
+HOME=$PROJECTS_DIR/home
 
-#cd $HOME
+repos=(
+	'aws-sigv4'
+	'aws-sdk-js-on-lambda'
+	'aws-lambda-ffmpeg'
+	'aws-cloudformation-cognito-identity-pool'
+	'IDL-Definitions'
+	'bower-register-github-org-repos'
+	'dashboard'
+)
 
-#git clone git@github.com:$GH_USERNAME/$REPO.git
+cd $HOME
+
+for repo in $repos; do
+	git clone git@github.com:${GH_USERNAME}/${repo}.git
+done
